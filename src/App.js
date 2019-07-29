442504792cf091c9ea571b751addc7115a337bbe
unknown <abhishek.bansal@BLAZECLAN.IN>
2019-07-29 17:04:40 +0530

    simple change

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Basic React App
+          Basic React App...
         </p>
         <a
           className="App-link"
